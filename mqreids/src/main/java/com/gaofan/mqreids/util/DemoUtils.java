a2cc5321c5b32ca87132ca55333efe84ce8f4b78
gaofan1989 <13924285707@163.com>
2022-03-16 18:02:23 +0800

    demoUtil

---
+++ mqreids/src/main/java/com/gaofan/mqreids/util/DemoUtils.java
@@ -0,0 +1,12 @@
+package com.gaofan.mqreids.util;
+
+/**
+ * @author gaofan
+ * @create 2022-03-16 18:01
+ */
+public class DemoUtils {
+
+    public static String getStr(String  name) {
+        return name.toUpperCase();
+    }
+}
